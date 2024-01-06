384bc0dba51c86b3f33c194bc0ad663745c5fe38
Shreyash Anpat <57129497+ShreyashAnpat@users.noreply.github.com>
2024-01-06 14:00:25 +0530

    added new files

--- home.java
+++ home.java
@@ -5,6 +5,10 @@ public class home {
 
     public static void main(String[] args) {
         System.out.println("this is Main Class  file which run by Jenkins ");
-        System.out.println("Shreyash Anpat");
+        
+        student student = new student() ;
+        student.studentCount();
+
+
     }
 } 
---
+++ student.java
@@ -0,0 +1,11 @@
+public class student {
+    
+    public student (){
+        System.out.println("Student Class called.");
+    }
+
+    public void studentCount(){
+        System.out.println("Totel Student -------> " + 100);
+    }
+
+}
